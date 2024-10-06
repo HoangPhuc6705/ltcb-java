69c6ff2da2fcf2b6aa2f740a46575a03fb86c477
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2024-10-06 21:57:29 +0700

    Basic Java

--- src/day2/TreeNode.java
+++ src/day2/TreeNode.java
@@ -23,10 +23,26 @@ public class TreeNode {
 		this.right = right;
 	}
 
-	public void isSymmetric(TreeNode root) {
+	public boolean isSymmetric(TreeNode root) {
 		ArrayList<TreeNode> level = new ArrayList<TreeNode>();
 		Queue<TreeNode> q = new LinkedList<TreeNode>();
 		BFS(level, q, root);
+		int start = 1;
+		int end = 2;
+		ArrayList<ArrayList<TreeNode>> B_Tree = new ArrayList<ArrayList<TreeNode>>();
+		while (end <= level.size()) {
+			B_Tree.add(new ArrayList<TreeNode>());
+			for (int i = start; i <= end; ++i) {
+				B_Tree.get(i).add(level.get(i));
+			}
+			
+			
+			
+			start = end + 1;
+			end = (end + 1) * 2;
+		}
+		
+		return true;
 	}
 
 	public void BFS(ArrayList<TreeNode> level, Queue<TreeNode> q, TreeNode node) {
